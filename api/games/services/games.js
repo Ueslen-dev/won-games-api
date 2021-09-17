d2cf4959746ba51ccbaba05f001824c2ea8935df
ueslen santos <ueslencriacao@gmail.com>
2021-09-16 23:25:25 -0300

    desenvolvendo scrapping de dados

--- api/games/services/games.js
+++ api/games/services/games.js
@@ -5,6 +5,22 @@
  * to customize this service
  */
 const axios = require("axios");
+const slugify = require("slugify");
+
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating: "BR0",
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
 
 module.exports = {
   populate: async (params) => {
@@ -13,6 +29,11 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(products, "produtos");
+    console.log(await getGameInfo(products[0].slug));
+
+    await strapi.services.publisher.create({
+      name: products[0].publisher,
+      slug: slugify(products[0].publisher).toLowerCase(),
+    });
   },
 };
